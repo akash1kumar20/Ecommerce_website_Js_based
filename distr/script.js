91474e8f40b80237bafbbaf51c599c2afe7b73b8
akash1kumar20 <akashkumar2101sm@gmail.com>
2024-06-03 18:32:29 +0530

    home page styling complete

---
+++ distr/script.js
@@ -0,0 +1,15 @@
+function extendedMenu() {
+  document.querySelector("#extendMenu").classList.toggle("hidden");
+}
+function userProfileExpand() {
+  document.querySelector("#userProfile").classList.toggle("hidden");
+}
+function menSectionExp() {
+  document.querySelector("#menSection").classList.toggle("hidden");
+}
+function womenSectionExp() {
+  document.querySelector("#womenSection").classList.toggle("hidden");
+}
+function searchSpanExp() {
+  document.querySelector("#SearchSpan").classList.toggle("hidden");
+}
