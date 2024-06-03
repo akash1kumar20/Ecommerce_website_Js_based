2a5a54e4503fe674e1ab02f3c033b162844ccb05
akash1kumar20 <akashkumar2101sm@gmail.com>
2024-06-03 20:01:48 +0530

    Women section added, product added dynamically

--- distr/script.js
+++ distr/script.js
@@ -36,3 +36,99 @@ imgElements.forEach((element) => {
     formElement.classList.toggle("hidden");
   });
 });
+
+onLoad();
+
+function onLoad() {
+  displayProducts();
+}
+
+//to display products on women page using js
+function displayProducts() {
+  let productContainer = document.querySelector("#productContainer");
+  //if we don't find the container, then we will return immediately from here
+  if (!productContainer) {
+    return;
+  }
+  let innerHtml = "";
+  products.forEach((product) => {
+    innerHtml += `
+<div class="bg-slate-300 px-10 py-4 rounded-xl">
+  <img
+    src="${product.image}"
+    class="h-[400px] w-[300px] drop-shadow-2xl rounded-xl"
+  />
+  <div>
+    <span class="bg-white py-1 border border-black absolute -translate-y-9 rounded-lg px-1 ms-2 text-sm opacity-70">
+      ${product.rating.stars} <i class="fa-solid fa-star" style="color: #74c0fc"></i> | ${product.rating.count}
+    </span>
+
+    <h5 class="uppercase font-semibold text-lg">${product.company}</h5>
+
+    <p class="capitalize text-sm text-slate-600">
+      ${product.item_name}
+    </p>
+
+    <p class="space-x-1">
+      <span class="font-semibold">Rs. ${product.current_price}</span>
+      <span class="text-slate-500 text-sm line-through">Rs. ${product.original_price}</span>
+      <span class="text-orange-600 text-sm"> (${product.discount_percentage}% OFF)</span>
+    </p>
+     <!-- size cart -->
+              <form >
+                <p class="flex mt-2 gap-x-5">
+                  <span class="border border-black p-1"
+                    ><input
+                      type="text"
+                      value="XS"
+                      class="inputStyleMan"
+                      readonly
+                      name="size"
+                  /></span>
+                  <span class="border border-black p-1"
+                    ><input
+                      value="S"
+                      type="text"
+                     class="inputStyleMan"
+                      readonly
+                      name="size"
+                  /></span>
+                  <span class="border border-black p-1"
+                    ><input
+                      value="MD"
+                      type="text"
+                       class="inputStyleMan"
+                      readonly
+                      name="size"
+                  /></span>
+                  <span class="border border-black p-1"
+                    ><input
+                      value="LG"
+                      type="text"
+                     class="inputStyleMan"
+                      readonly
+                      name="size"
+                  /></span>
+                  <span class="border border-black p-1"
+                    ><input
+                      value="XL"
+                      type="text"
+                     class="inputStyleMan"
+                      readonly
+                      name="size"
+                  /></span>
+                </p>
+      <div class="mt-3">
+        <button
+          class="uppercase bg-blue-600 text-white px-6 py-2 rounded-md"
+          onclick="addToBag( ${product.id})"
+        >
+          Add to Bag
+        </button>
+      </div>
+    </form>
+  </div>
+</div> `;
+  });
+  productContainer.innerHTML = innerHtml;
+}
---
+++ distr/womenSection_dummyData.js
@@ -0,0 +1,86 @@
+let products = [
+  {
+    id: "001",
+    image:
+      "https://plus.unsplash.com/premium_photo-1673977132933-2a028c2f05a8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8d29tZW4lMjBkcmVzc3xlbnwwfHwwfHx8MA%3D%3D",
+    company: "Berrylush",
+    item_name: "Floral Bustier Dress",
+    original_price: 1799,
+    current_price: 989,
+    discount_percentage: 45,
+    rating: {
+      stars: 3.9,
+      count: 2700,
+    },
+  },
+  {
+    id: "002",
+    image:
+      "https://images.unsplash.com/photo-1495385794356-15371f348c31?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdvbWVuJTIwZHJlc3N8ZW58MHx8MHx8fDA%3D",
+    company: "bitterlime",
+    item_name: "women georgette maxi dress",
+    original_price: 3999,
+    current_price: 1399,
+    discount_percentage: 65,
+    rating: {
+      stars: 4,
+      count: 207,
+    },
+  },
+  {
+    id: "003",
+    image:
+      "https://images.unsplash.com/photo-1563178406-4cdc2923acbc?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHdvbWVuJTIwZHJlc3N8ZW58MHx8MHx8fDA%3D",
+    company: "DressBerry",
+    item_name: "women shirt dress",
+    original_price: 2099,
+    current_price: 734,
+    discount_percentage: 65,
+    rating: {
+      stars: 4.3,
+      count: 262,
+    },
+  },
+  {
+    id: "004",
+    image:
+      "https://images.unsplash.com/photo-1595272832990-82e5cd240255?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8d29tZW4lMjBqZWFuc3xlbnwwfHwwfHx8MA%3D%3D",
+    company: "Carlton London",
+    item_name: "women slim fit jeans",
+    original_price: 1045,
+    current_price: 606,
+    discount_percentage: 42,
+    rating: {
+      stars: 4.5,
+      count: 1200,
+    },
+  },
+  {
+    id: "005",
+    image:
+      "https://images.unsplash.com/photo-1594633312681-425c7b97ccd1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8d29tZW4lMjBqZWFuc3xlbnwwfHwwfHx8MA%3D%3D",
+    company: "H&M",
+    item_name: "Women stylish pent",
+    original_price: 1999,
+    current_price: 1799,
+    discount_percentage: 10,
+    rating: {
+      stars: 4,
+      count: 14000,
+    },
+  },
+  {
+    id: "006",
+    image:
+      "https://images.unsplash.com/photo-1570397369306-f42d7dbce359?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHdvbWVuJTIwamVhbnN8ZW58MHx8MHx8fDA%3D",
+    company: "Roadster",
+    item_name: "High waist jeans",
+    original_price: 1899,
+    current_price: 930,
+    discount_percentage: 51,
+    rating: {
+      stars: 4.1,
+      count: 3000,
+    },
+  },
+];
